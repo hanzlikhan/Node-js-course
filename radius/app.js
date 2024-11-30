6092d6cbd7a4b7851912f1f7fc4730a96f4e59fb
hanzlikhan <khangormani79@gmail.com>
2024-11-30 21:09:42 +0500

    init

--- radius/app.js
+++ radius/app.js
@@ -4,9 +4,9 @@
 const { radius } = require("./constants");
 
 // Import the calculateArea function from circleUtils.js
-const { calculateArea } = require("./circleUtils");
+// const { calculateArea } = require("./circleUtils");
 
-// Use the function and display the result
-const area = calculateArea(radius);
+// // Use the function and display the result
+// const area = calculateArea(radius);
 
 console.log(`The area of a circle with radius ${radius} is ${area.toFixed(2)}.`);
