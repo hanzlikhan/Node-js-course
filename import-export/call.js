d9d0feb730f492bea269dbf351c00f725dd51bb5
hanzlikhan <khangormani79@gmail.com>
2024-12-01 18:48:32 +0500

    init

---
+++ import-export/call.js
@@ -0,0 +1,12 @@
+
+// importing default method 
+const names = require('./dec')
+const sayHi = require('./func')
+const data = require('./alternative-flavor')
+// console.log(data);
+
+require('./mind_grand')
+// sayHi('susan');
+// sayHi(names.john);
+// sayHi(names.peter);
+
