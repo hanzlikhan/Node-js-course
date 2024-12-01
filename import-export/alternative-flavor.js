d4abbd8d22a3f961971fb82681772b819e088bda
hanzlikhan <khangormani79@gmail.com>
2024-12-01 18:48:26 +0500

    init

---
+++ import-export/alternative-flavor.js
@@ -0,0 +1,11 @@
+// this is the hardest thing in the programming
+
+// here we are setting up a property 
+module.exports.items = ['item1', 'item2'];
+const person = {
+  name: 'hanzla',
+
+}
+
+// set up another property 
+module.exports.singlePerson = person
