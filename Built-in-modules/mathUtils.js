37a2cc255a914b7c7642796e4d76b44fc80a1fe6
hanzlikhan <khangormani79@gmail.com>
2024-12-02 19:10:30 +0500

    module

---
+++ Built-in-modules/mathUtils.js
@@ -0,0 +1,10 @@
+// A simple local module with math functions
+function add(a, b) {
+    return a + b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+module.exports = { add, multiply }; // Export the functions
