c1f567e57b3593dcd0c81cc260be46d03acaafce
hanzlikhan <khangormani79@gmail.com>
2024-12-02 19:10:10 +0500

    module

---
+++ Built-in-modules/app.js
@@ -0,0 +1,5 @@
+// Import your custom module
+const math = require('./mathUtils');
+
+console.log('Sum:', math.add(4, 5)); // Output: Sum: 9
+console.log('Product:', math.multiply(4, 5)); // Output: Product: 20
