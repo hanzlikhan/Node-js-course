28e3d5cda7a04294f11795a4ce2433960aa08a4a
hanzlikhan <khangormani79@gmail.com>
2024-12-05 14:10:53 +0500

    init

---
+++ Built-in-modules/http-module/json.js
@@ -0,0 +1,17 @@
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/api') {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.write(JSON.stringify({ message: 'Hello, JSON!' }));
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.write('Not Found');
+    }
+    res.end();
+});
+
+server.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
