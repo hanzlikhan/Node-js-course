4a3ea29d03a9f7ed81b73aa4a0a3b2ea3cd3fc6a
hanzlikhan <khangormani79@gmail.com>
2024-12-05 14:10:40 +0500

    init

---
+++ Built-in-modules/write/write.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+fs.writeFile('output.txt', 'Hello, Node.js!', (err) => {
+    if (err) {
+        console.error('Error writing file:', err);
+        return;
+    }
+    console.log('File written successfully!');
+});
