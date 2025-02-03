6f3a3af823e63fc9f56a4879a0286a10fc5f771c
hanzlikhan <khangormani79@gmail.com>
2025-02-03 17:12:52 +0500

    init

---
+++ Delta-backend/script.js
@@ -0,0 +1,27 @@
+const info = require("./fruits")
+
+console.log(info);
+
+
+
+
+
+
+
+
+
+
+// const packageName = require('./math');
+// console.log(packageName.sum(2,3));
+// console.log(packageName.product(2,3));
+// console.log(packageName.sub(2,3));
+// console.log(packageName.PI);
+// console.log(packageName.g);
+
+
+// let args = process.argv;
+// for(let i=2; i<args.length; i++) {
+//   console.log("hello to " , args[i])
+    
+//   }
+
