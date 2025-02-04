026f182082c5684861da09f60e4dc09651f8cc47
hanzlikhan <khangormani79@gmail.com>
2025-02-04 18:29:25 +0500

    init

--- Miscellaneous/Backend/index.js
+++ Miscellaneous/Backend/index.js
@@ -2,22 +2,39 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000; // Allow dynamic port assignment
 
+//Middleware for the post request 
+
+// handle post requests
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+// GET route for /register
 app.get('/register', (req, res) => {
-  res.send('standard GET response from server');
+  let {user , password} = req.query   // all info which is sent from client side through get method is stored in req.query
+  res.send(`Standard GET response, Welcome ${user}`);
 });
 
+// POST route for /register
 app.post('/register', (req, res) => {
-  res.send('standard POST response from server');
+// we can't use req.query here coz we have to pass sensitive information which is vidible in this method and we can't use req.query here.
+
+// can use req.body here but it is not specify the information that's why we need to use other two lines 
+  let {user , password} = req.body;
+  res.send('Standard POST response ' + user);
 });
 
-// Handle port in use error
-const server = app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-}).on('error', (err) => {
-  if (err.code === 'EADDRINUSE') {
-    console.log(`Port ${port} is in use. Trying a different port...`);
-    server.listen(port + 1);
-  } else {
-    console.error(err);
-  }
-});
+// Start server with error handling
+const startServer = (port) => {
+  const server = app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  }).on('error', (err) => {
+    if (err.code === 'EADDRINUSE') {
+      console.log(`Port ${port} is in use. Trying a different port...`);
+      startServer(port + 1);
+    } else {
+      console.error('Server error:', err);
+    }
+  });
+};
+
+startServer(port);
